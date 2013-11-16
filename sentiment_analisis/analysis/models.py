b554820583d6cb81f21fb0d39ef1e74dd406ba9d
Bernhard <e1026883@student.tuwien.ac.at>
2013-11-16 11:13:01 +0100

    added apps

---
+++ sentiment_analisis/analysis/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class Feed(models.Model):
+    title = models.CharField(max_length=500)
+    link = models.URLField()
+    content = models.TextField()
+
+class Keyword(models.Model):
+    text = models.CharField(max_length=500)
+
+class Worker(models.Model):
+    score = models.IntegerField(default=0)
+    blocked = models.BooleanField('blocked')
+
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+	('NS', 'not started'),
+	('S', 'started'),
+	('D', 'done')
+    ) 
+    status = models.CharField(max_length=1,choices=STATUS_CHOICES,default='NS')
+    pub_date = models.DateTimeField('date published')
+    com_date = models.DateTimeField('date completed')
+    orphaned = models.BooleanField('Orphaned')
+    price = models.IntegerField(default=0)
+    callback_uri = models.CharField(max_length=200)
+    question = models.TextField()
+    answer = models.TextField()
+    feed = models.ForeignKey(Feed)
+    keywords = models.ManyToManyField(Keyword)
+    worker = models.ForeignKey(Worker)   
+         
+class Sentiment(models.Model):
+    keyword = models.ForeignKey(Keyword)
+    worker = models.ForeignKey(Worker)
+    score = models.IntegerField(default=0)    
+
+
+# Create your models here.
