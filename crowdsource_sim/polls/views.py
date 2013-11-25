ead373b5185a9967ef93ee4a744adcb7c6d169d3
Bernhard <e1026883@student.tuwien.ac.at>
2013-11-25 21:59:13 +0100

    Create Task template out of Task data finished

--- crowdsource_sim/polls/views.py
+++ crowdsource_sim/polls/views.py
@@ -13,13 +13,35 @@ from polls.models import Task, Worker
 
 
 def index(request):
-    latest_poll_list = Task.objects.all().order_by('-id')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
+#    latest_poll_list = Task.objects.all().order_by('-id')[:5]
+#    context = {'latest_poll_list': latest_poll_list}
+    test  = 'Meine UserId'
+    test2 = 'Das ist noch ein Test :-)'
+    headers = {'Header1', 'Header2', 'Header3'}
+    context = {'userid' : test, 'question': 'Meine Frage?', 'header' : 'Mein Header !', 'input' : 'Input is das ;-)', 'taskid' : '1', 'headers' : headers}
+    return render(request, 'polls/task_temp.html', context)
 
 def detail(request, task_id):
     task = get_object_or_404(Task, id=task_id)
-    return render(request, 'polls/detail.html', {'task': task})
+   
+    
+    try:
+      decoded = json.loads(task.data)
+      headers = {'Header1', 'Header2', 'Header3'}
+      print "JSON parsing example: ", decoded['properties']['taskDescription']['description']
+      taskDescription = decoded['properties']['taskDescription']['description']
+      taskTitle = decoded['properties']['taskTitle']['description']
+      taskInput = decoded['properties']['input']['description']
+      additionalInput = decoded['properties']['additionalInput']['description']
+      additional_header = decoded['properties']['additionalInputHeader']['description']
+      headers = decoded['properties']['resultColumns']['description']
+      #context = {'userid' : 'iwas', 'question': 'Meine Frage?', 'header' : 'Mein Header !', 'input' : 'Input is das ;-)', 'taskid' : '1', 'headers' : headers}
+      context = {'userid' : 'MyUser', 'question' : taskDescription, 'header' : taskTitle, 'input' : taskInput, 'additional_input' : additionalInput, 'additional_header' : additional_header, 'taskid': task_id, 'headers' : headers}
+      return render(request, 'polls/task_temp.html', context)  	
+    except (ValueError, KeyError, TypeError):  
+      print "JSON format error"
+
+    return HttpResponse("Something went wrong with JSON")
 
 def results(request, task_id):
     return HttpResponse("You're looking at the results of poll %s." % task_id)
