9bc356f1ffea8c1ac535baa485bdc3dfa5942ea8
Bernhard <e1026883@student.tuwien.ac.at>
2013-11-25 22:24:13 +0100

    added unique column and rows and parsing for answer field

--- crowdsource_sim/polls/views.py
+++ crowdsource_sim/polls/views.py
@@ -27,7 +27,7 @@ def detail(request, task_id):
     
     try:
       decoded = json.loads(task.data)
-      headers = {'Header1', 'Header2', 'Header3'}
+    #  headers = {'Header1', 'Header2', 'Header3'}
       print "JSON parsing example: ", decoded['properties']['taskDescription']['description']
       taskDescription = decoded['properties']['taskDescription']['description']
       taskTitle = decoded['properties']['taskTitle']['description']
@@ -51,16 +51,26 @@ def submit(request, task_id):
       #print request.POST['keyword2']
       
       t = get_object_or_404(Task, id=task_id)
+
+      
       try:
         decoded = json.loads(t.data)
- 		
+
+	headers = decoded['properties']['resultColumns']['description']
+	buff = ""
+	for column in headers:
+          buff += column+": \n"
+	  for row in range(0,9):
+	    buff += "Row"+str(row)+":  "+request.POST[column+'_'+str(row)]+"\n"
+
+		
        # pretty printing of json-formatted string
-        print json.dumps(decoded, sort_keys=True, indent=4)
- 
-        buff = request.POST['keyword1']+' ; '+request.POST['type1']+' ,\n '
-        buff += request.POST['keyword2']+' ; '+request.POST['type2']+' ,\n '
-        buff += request.POST['keyword3']+' ; '+request.POST['type3']+' ,\n '
-        print buff
+      #  print json.dumps(decoded, sort_keys=True, indent=4)
+ 	
+      #  buff = request.POST['keyword1']+' ; '+request.POST['type1']+' ,\n '
+      #  buff += request.POST['keyword2']+' ; '+request.POST['type2']+' ,\n '
+      #  buff += request.POST['keyword3']+' ; '+request.POST['type3']+' ,\n '
+      #  print buff
         t.answer = buff
 	t.save()
 	#print "JSON parsing example: ", decoded['title']
@@ -68,8 +78,8 @@ def submit(request, task_id):
  	
       except (ValueError, KeyError, TypeError):
         print "JSON format error"
-
-      return HttpResponse('Ausgabe: ')
+	
+      return HttpResponse('Task finished successfully ')
        
 
     return HttpResponse('Somethng is wrong: Not POST Request Methode') 
