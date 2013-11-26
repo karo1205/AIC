5b360ba4f066e3b401aad9814a1331391dc845af
Bernhard <e1026883@student.tuwien.ac.at>
2013-11-26 22:36:53 +0100

    Created Sentiment Analysis View/Template

---
+++ sentiment_analisis/analysis/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from analysis import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.query, name='query'),
+    url(r'^result/$', views.result, name='result')
+)
--- sentiment_analisis/analysis/views.py
+++ sentiment_analisis/analysis/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+
+from django.shortcuts import render, get_object_or_404
+
+from analysis.models import Keyword, Sentiment
+
+def query(request):
+    
+    context = {'userid' : 'MyUser', 'keywords' : Keyword.objects.all()}
+    return render(request, 'analysis/query.html', context)
+
+def result(request):
+    
+    sentiments = Sentiment.objects.filter(keyword = request.POST['selectbox'])	
+
+    context = {'userid' : 'MyUser', 'sentiments' : sentiments, 'keyword' : Keyword.objects.get(id=request.POST['selectbox'])}
+    return render(request, 'analysis/result.html', context)
 
 # Create your views here.
