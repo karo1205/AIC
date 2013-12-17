0a587ed6a1fb59339943f6d4ed1fedb2df86ded2
Bernhard <e1026883@student.tuwien.ac.at>
2013-12-17 20:49:37 +0100

    added Order to model

--- sentiment_analisis/analysis/models.py
+++ sentiment_analisis/analysis/models.py
@@ -23,6 +23,11 @@ class Worker(models.Model):
     blocked = models.BooleanField('blocked')
     worker_uri = models.CharField(max_length=200, default='NULL')
 
+class Order(models.Model):
+    keyword = models.ForeignKey(Keyword)
+    budgetlimit = models.IntegerField(default=0)
+    start_date = models.DateTimeField('start date', default=datetime.datetime(2000, 1, 1, 1, 1, 1))
+    end_date = models.DateTimeField('end date', default=datetime.datetime(2000, 1, 1, 1, 1, 1))
 
 class Task(models.Model):
     STATUS_CHOICES = (
