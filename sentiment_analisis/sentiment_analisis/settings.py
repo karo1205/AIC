136040e042de789cf0ff685a73e564e8982cbc51
Yodave <yodave85@gmail.com>
2014-01-28 23:46:11 +0100

    added test graph

--- sentiment_analisis/sentiment_analisis/settings.py
+++ sentiment_analisis/sentiment_analisis/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'tastypie',
     'django_cron',
     'south',
+    'chart_tools',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
